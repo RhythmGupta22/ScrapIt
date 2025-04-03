bd95b8862ba9621d4345a42ed27f39e4898e8599
RhythmGupta22 <rhythm.gupta360@gmail.com>
2025-04-03 20:06:51 +0530

    build file updated

--- lib/screens/login/views/login_screen.dart
+++ lib/screens/login/views/login_screen.dart
@@ -1,132 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:uicons/uicons.dart';
-// import 'package:scrap_it/screens/login/views/login_with_email.dart';
-// import 'package:scrap_it/screens/login/views/login_with_phone.dart';
-// import 'package:scrap_it/screens/signup/views/signup_screen.dart';
-// import 'package:scrap_it/sharedWidgets/custom_bordered_button.dart';
-// import 'package:scrap_it/sharedWidgets/custom_filled_button.dart';
-//
-// import '../controller/login_controller.dart';
-//
-// class LoginScreen extends StatelessWidget {
-//   LoginScreen({super.key});
-//   final _loginController = Get.find<LoginController>();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: SingleChildScrollView(
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 Center(
-//                   child: Text(
-//                     'Login',
-//                     style: TextStyle(
-//                       fontSize: 22,
-//                       fontWeight: FontWeight.bold,
-//                     ),
-//                   ),
-//                 ),
-//                 SizedBox(
-//                   height: 35,
-//                 ),
-//                 Image.asset(
-//                   'assets/images/loginScreen/login_screen.png',
-//                   height: 300,
-//                   width: 300,
-//                 ),
-//                 SizedBox(
-//                   height: 20,
-//                 ),
-//                 CustomFilledButton(
-//                   title: 'Login With Phone',
-//                   onPressed: () {
-//                     Get.to(() => LoginWithPhone());
-//                   },
-//                   icon: UIcons.regularRounded.mobile_notch,
-//                 ),
-//                 SizedBox(
-//                   height: 20,
-//                 ),
-//                 CustomBorderedButton(
-//                   title: 'Login With Email',
-//                   onPressed: () {
-//                     Get.to(() => LoginWithEmail());
-//                   },
-//                   icon: UIcons.regularRounded.envelope,
-//                 ),
-//                 SizedBox(
-//                   height: 20,
-//                 ),
-//                 Row(
-//                   children: const [
-//                     Expanded(
-//                       child: Divider(
-//                         thickness: 2,
-//                       ),
-//                     ),
-//                     Padding(
-//                       padding: EdgeInsets.symmetric(horizontal: 16),
-//                       child: Text("OR"),
-//                     ),
-//                     Expanded(
-//                       child: Divider(
-//                         thickness: 2,
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//                 SizedBox(
-//                   height: 20,
-//                 ),
-//                 CustomBorderedButton(
-//                   title: 'Login With Google',
-//                   onPressed: () {
-//                     _loginController.signInWithGoogle();
-//                   },
-//                   icon: UIcons.brands.google,
-//                 ),
-//                 SizedBox(
-//                   height: 20,
-//                 ),
-//                 GestureDetector(
-//                   onTap: () {
-//                     //* Navigate to Sign Up Screen
-//                     Get.to(() => SignUpScreen());
-//                   },
-//                   child: RichText(
-//                     text: TextSpan(
-//                       text: 'Don\'t have an account? ',
-//                       style: TextStyle(
-//                         color: Colors.black,
-//                         fontSize: 16,
-//                       ),
-//                       children: const [
-//                         TextSpan(
-//                           text: 'Sign Up',
-//                           style: TextStyle(
-//                             decoration: TextDecoration.underline,
-//                             color: Colors.black,
-//                             fontSize: 16,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uicons/uicons.dart';
@@ -136,11 +7,14 @@ import 'package:scrap_it/screens/signup/views/signup_screen.dart';
 import 'package:scrap_it/sharedWidgets/custom_bordered_button.dart';
 import 'package:scrap_it/sharedWidgets/custom_filled_button.dart';
 
+import '../../../constants/colors.dart';
 import '../controller/login_controller.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
   final _loginController = Get.find<LoginController>();
+  final RxBool isSeller = false.obs;
+  final RxBool isCollector = false.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -215,27 +89,84 @@ class LoginScreen extends StatelessWidget {
                             icon: UIcons.brands.google,
                           ),
                           SizedBox(height: 20),
-                          GestureDetector(
-                            onTap: () {
-                              Get.to(() => SignUpScreen());
-                            },
-                            child: RichText(
-                              text: TextSpan(
-                                text: 'Don\'t have an account? ',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                ),
-                                children: const [
-                                  TextSpan(
-                                    text: 'Sign Up',
-                                    style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
+                          Obx(() => Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Checkbox(
+                                        checkColor: kSecondaryColor,
+                                        activeColor: kPrimaryColor,
+                                        value: isSeller.value,
+                                        onChanged: (value) {
+                                          isSeller.value = value!;
+                                          if (value) {
+                                            isCollector.value = false;
+                                          }
+                                        },
+                                      ),
+                                      Text(
+                                        'Seller',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: kPrimaryColor,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(width: 20),
+                                  Row(
+                                    children: [
+                                      Checkbox(
+                                        checkColor: kSecondaryColor,
+                                        activeColor: kPrimaryColor,
+                                        value: isCollector.value,
+                                        onChanged: (value) {
+                                          isCollector.value = value!;
+                                          if (value) {
+                                            isSeller.value = false;
+                                          }
+                                        },
+                                      ),
+                                      Text(
+                                        'Collector',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: kPrimaryColor,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ],
+                              )),
+                          SizedBox(height: 40),
+                          Center(
+                            child: GestureDetector(
+                              onTap: () {
+                                Get.to(() => SignUpScreen());
+                              },
+                              child: RichText(
+                                text: TextSpan(
+                                  text: 'Don\'t have an account? ',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                  children: const [
+                                    TextSpan(
+                                      text: 'Sign Up',
+                                      style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
